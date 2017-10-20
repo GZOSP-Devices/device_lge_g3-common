3315867f8e21362cd6a768f4c30b566db5de8ace
bryan2894 <bryan2894@gmail.com>
2017-10-20 15:34:42 -0400

    g3: build custom legacy camera provider

--- g3.mk
+++ g3.mk
@@ -217,6 +217,7 @@ PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl \
     android.hardware.vibrator@1.0-impl.lineage \
     android.hardware.camera.provider@2.4-impl \
+    camera.device@1.0-impl.legacy \
     android.hardware.sensors@1.0-impl \
     android.hardware.gatekeeper@1.0-impl \
     android.hardware.power@1.0-impl \
